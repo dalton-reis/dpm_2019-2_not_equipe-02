6aaeeaeb0da7535b3c38569690ee14469d3ab073
AlexSerodio <alequis01@gmail.com>
2019-11-21 17:18:32 -0300

    ajustado tamanho das plantas

--- Assets/Scripts/PlantSpawn.cs
+++ Assets/Scripts/PlantSpawn.cs
@@ -12,6 +12,10 @@ public class PlantSpawn : MonoBehaviour {
 
     public void InstantiatePlant() {
         GameObject newPlant = biomaController.InstantiatePlant(new Vector2(transform.position.x, transform.position.y+50));
+        
+        Plant plant = newPlant.GetComponent<PlantController>().plant;
+        newPlant.transform.position = new Vector2(transform.position.x, transform.position.y + plant.height/2 - 90);
+
         gameManager.ShowPlantSpawns(false);
         
         newPlant.GetComponent<PlantController>().SpawnPoint = gameObject;
